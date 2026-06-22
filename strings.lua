7d7e21cd8ac252fee9a4af27e755aaa9973bac19
Patryk Rakowski <patrykr2001@gmail.com>
2026-06-22 17:45:58 +0200

    Rest of the chapter 4 excersises

--- strings.lua
+++ strings.lua
@@ -1,4 +1,4 @@
-xml = [===[
+local xml = [===[
 <![CDATA[
   Hello world
 ]]>
@@ -6,11 +6,14 @@ xml = [===[
 
 print(xml)
 
-function insert(a, index, b)
+local function insert(a, index, b)
 	return a:sub(0, index - 1) .. b .. a:sub(index)
 end
 
-function insertUTF8(a, index, b)
+print(insert("hello world", 1, "start: "))
+print(insert("hello world", 7, "small "))
+
+local function insertUTF8(a, index, b)
 	local bytePos
 	if index <= 1 then
 		bytePos = 1
@@ -20,8 +23,31 @@ function insertUTF8(a, index, b)
 	return a:sub(1, bytePos - 1) .. b .. a:sub(bytePos)
 end
 
-print(insert("hello world", 1, "start: "))
-print(insert("hello world", 7, "small "))
+print(insertUTF8("ação", 5, "!"))
 
-print(insertUTF8("hello world", 1, "start: "))
-print(insertUTF8("hello world", 7, "small "))
+local function remove(a, pos, len)
+	return a:sub(0, pos - 1) .. a:sub(pos + len)
+end
+
+print(remove("hello world", 7, 4))
+
+local function removeUTF8(a, pos, len)
+	return a:sub(0, utf8.offset(a, pos - 1)) .. a:sub(utf8.offset(a, pos + len))
+end
+
+print(removeUTF8("ação", 2, 2))
+
+local function ispali(a)
+	return a == a:reverse()
+end
+
+print(ispali("step on no pets"))
+print(ispali("banana"))
+
+local function ispaliCleaned(a)
+	local cleaned = a:lower():gsub("[^%w]", "")
+	return cleaned == cleaned:reverse()
+end
+
+print(ispaliCleaned("step on no pets"))
+print(ispaliCleaned("banana"))
